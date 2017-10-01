849e2aaae07b2e40bc542597cae78e26481b8752
Ameen Ruhul Mohammad <ameensunny242@gmail.com>
2017-10-01 16:45:01 +0600

    TIP120 Transistor

---
+++ ArduinoMotorWithTransistor/ArduinoMotorWithTransistor.ino
@@ -0,0 +1,37 @@
+
+
+// the transistor which controls the motor will be attached to digital pin 9
+int motorControl = 9;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // make the pushbutton's pin an input:
+
+
+  // make the transistor's pin an output:
+  pinMode(motorControl, OUTPUT);  
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+
+  // read the state of the button and check if it is pressed
+ 
+    // ramp up the motor speed
+    for(int x = 0; x <= 255; x++){
+      analogWrite(motorControl, x);
+      delay(50);
+    
+
+    // ramp down the motor speed
+    for(int x = 255; x >= 0; x--){
+      analogWrite(motorControl, x);
+      delay(50);
+    }    
+  }
+
+  delay(1);        // delay in between reads for stability
+}
+
+
+ 
